dba2abb66ec3f6c1319dc74e9f5de1ded6c4be1c
itadmin@polanerselections.com <itadmin@polanerselections.com>
2024-08-13 16:48:56 -0400

    Add Lambda function which logs status of SSM job completion

---
+++ functions/SalesPortalExport-JobCompletionHandler/app.py
@@ -0,0 +1,10 @@
+import json
+
+
+def lambda_handler(event, context):
+    # Log the received event
+    print("Received event: " + json.dumps(event, indent=2))
+
+    error_details = event.get('errorDetails', {})
+    if error_details:
+        raise Exception(f'Error processing a job: {error_details}')
